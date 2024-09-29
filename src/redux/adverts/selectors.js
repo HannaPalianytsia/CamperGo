bcaa5d05e75ec66501920cc6aeb995b753284f93
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-09-29 16:16:48 +0000

    Deploying to gh-pages from @ HannaPalianytsia/CamperGo@31d443867c63ad86b4dbbc582f420f1a49237585 🚀

--- src/redux/adverts/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectAdverts = (state) => state.adverts.items;
-export const selectIsAllAdvertsLoaded = (state) =>
-  state.adverts.isAllAdvertsLoaded;
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { advertsReducer } from './adverts/slice';
-
-export const store = configureStore({
-  reducer: {
-    adverts: advertsReducer,
-  },
-});
